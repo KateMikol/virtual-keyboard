34b41092aea758c837c2facca0a1dd4bfb43a1d7
KateMikol <gornovskayakt@gmail.com>
2023-04-22 15:39:41 +0300

    feat: add text input by pressing the key

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-// let arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// let arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let arr_EN = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+const arrEN = [
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL',
+  'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'ENTER',
+  'Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift',
+  'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→',
+];
 
-let header = document.createElement('header');
+const exceptionItems = ['Tab', 'Backspace', 'DEL', 'Caps Lock', 'ENTER', 'Shift',
+  'Ctrl', 'Win', 'Alt'];
+
+const header = document.createElement('header');
 header.className = '';
 
-let main = document.createElement('main');
+const main = document.createElement('main');
 main.className = '';
 
-let textArea = document.createElement('textarea');
-textArea.className = '';
-let keyboard = document.createElement('section');
+const textArea = document.createElement('textarea');
+textArea.className = 'text__area';
+const keyboard = document.createElement('section');
 keyboard.className = 'keyboard';
-let title = document.createElement('h1');
-let titleText = document.createTextNode('Virtual Keyboard');
+const title = document.createElement('h1');
+const titleText = document.createTextNode('Virtual Keyboard');
 title.appendChild(titleText);
 header.appendChild(title);
 
@@ -22,4 +29,56 @@ document.body.appendChild(main);
 main.appendChild(textArea);
 main.appendChild(keyboard);
 
+function generateKeyboardButtons(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const keyboardItem = document.createElement('div');
+    if (arr[i] === ' ') {
+      keyboardItem.className = 'keyboard__item keyboard__item_space';
+    } else {
+      keyboardItem.className = 'keyboard__item';
+    }
+    const keyboardItemContent = document.createElement('p');
+    const keyboardItemContentText = document.createTextNode(arr[i]);
+    keyboard.appendChild(keyboardItem);
+    keyboardItem.appendChild(keyboardItemContent);
+    keyboardItemContent.append(keyboardItemContentText);
+  }
+}
 
+generateKeyboardButtons(arrEN);
+
+function getValueOfClickedItem(e) {
+  const targetKeyboardItem = e.target.closest('.keyboard__item');
+  if (targetKeyboardItem) {
+    const valueOfClickedItem = targetKeyboardItem.querySelector('p').innerHTML;
+    return valueOfClickedItem;
+  }
+  return undefined;
+}
+
+function getCursor() {
+  return textArea.selectionStart;
+}
+
+function onKeyboardClickHandler(e) {
+  const clickedItemValue = getValueOfClickedItem(e);
+  if (!clickedItemValue) {
+    return;
+  }
+  const cursorPosition = getCursor();
+  let textFromTextArea = textArea.value;
+  if (!exceptionItems.includes(clickedItemValue)) {
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition) + clickedItemValue
+    + textFromTextArea.slice(cursorPosition - 1 + clickedItemValue.length);
+
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+  }
+}
+
+keyboard.addEventListener('click', onKeyboardClickHandler);
+
+// document.addEventListener('keydown', (e) => {
+//   console.log(e.repeat);
+//   console.log(e.code);
+// });
