1b02d0b8b38c2e0b4e985fb682bd38912e9c405c
KateMikol <gornovskayakt@gmail.com>
2023-04-24 18:27:12 +0300

    feat: add pressing key from real keyboard

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const arren = [
   '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '\\', 'DEL',
   'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'ENTER',
-  'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '↑', 'Shift',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '↑', 'Shift',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→',
 ];
 const arrru = [
-  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL',
   'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'ENTER',
-  'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '↑', 'Shift',
+  'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '↑', 'Shift',
   'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→',
 ];
 
@@ -22,13 +22,27 @@ const letters = [
   'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Я', 'Ч', 'С',
   'М', 'И', 'Т', 'Ь', 'Б', 'Ю', 'й', 'ц', 'у', 'к', 'е', 'н', 'г',
   'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л',
-  'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю',
+  'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'ё', 'Ё',
+];
+const keyCodes = [
+  'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+  'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+  'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+  'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight',
 ];
 
-let keyboardLang = 'arrEN';
-
+function getCurrentLang() {
+  let currentLang = 'en';
+  if (localStorage.getItem('lang')) {
+    currentLang = localStorage.getItem('lang');
+  } else {
+    localStorage.setItem('lang', currentLang);
+  }
+  return currentLang;
+}
 const exceptionItems = ['Tab', 'Backspace', 'DEL', 'Caps Lock', 'ENTER', 'Shift',
-  'Ctrl', 'Win', 'Alt'];
+  'Ctrl', 'Win', 'Alt', 'Enter', 'Delete'];
 
 const header = document.createElement('header');
 header.className = '';
@@ -39,7 +53,7 @@ main.className = '';
 const textArea = document.createElement('textarea');
 textArea.className = 'text__area';
 const keyboard = document.createElement('section');
-keyboard.className = 'keyboard en';
+keyboard.className = `keyboard ${getCurrentLang()}`;
 const title = document.createElement('h1');
 const titleText = document.createTextNode('Virtual Keyboard');
 
@@ -51,6 +65,7 @@ main.appendChild(textArea);
 main.appendChild(keyboard);
 
 const queueFromClickedItems = [];
+let queueFromPressedKeys = [];
 
 function generateKeyboardButtons(arr) {
   for (let i = 0; i < arr.length; i++) {
@@ -61,6 +76,8 @@ function generateKeyboardButtons(arr) {
       keyboardItem.className = 'keyboard__item keyboard__item_CapsLock';
     } else if (arr[i] === 'ENTER') {
       keyboardItem.className = 'keyboard__item keyboard__item_enter';
+    } else if (arr[i] === 'Shift') {
+      keyboardItem.className = 'keyboard__item keyboard__item_shift';
     } else {
       keyboardItem.className = 'keyboard__item';
     }
@@ -72,7 +89,7 @@ function generateKeyboardButtons(arr) {
   }
 }
 
-generateKeyboardButtons(arren);
+generateKeyboardButtons(getCurrentLang() === 'en' ? arren : arrru);
 
 function getValueOfClickedItem(e) {
   const targetKeyboardItem = e.target.closest('.keyboard__item');
@@ -119,12 +136,14 @@ function changeLang() {
   if (keyboardElem.classList.contains('en')) {
     keyboardElem.classList.remove('en');
     keyboardElem.classList.add('ru');
+    localStorage.setItem('lang', 'ru');
     for (let i = 0; i < arrOfKeyBoardItems.length; i++) {
       arrOfKeyBoardItems[i].querySelector('p').innerHTML = arrru[i];
     }
   } else {
     keyboardElem.classList.remove('ru');
     keyboardElem.classList.add('en');
+    localStorage.setItem('lang', 'en');
     for (let i = 0; i < arrOfKeyBoardItems.length; i++) {
       arrOfKeyBoardItems[i].querySelector('p').innerHTML = arren[i];
     }
@@ -165,8 +184,14 @@ function getCursor() {
 
 function onKeyboardClickHandler(e) {
   const clickedItemValue = getValueOfClickedItem(e);
-  
   queueFromClickedItems.push(clickedItemValue);
+  if (queueFromClickedItems.length > 2) {
+    queueFromClickedItems.shift();
+  }
+  if (queueFromClickedItems[0] === 'Alt' && queueFromClickedItems[1] === 'Shift') {
+    changeLang();
+    // delPreviousClickedItem();
+  }
 
   if (!clickedItemValue) {
     return;
@@ -203,7 +228,7 @@ function onKeyboardClickHandler(e) {
     textArea.setSelectionRange(cursorPosition, cursorPosition);
   }
   if (clickedItemValue === 'Tab') {
-    const tabValue = '    ';
+    const tabValue = '\t';
     textFromTextArea = textFromTextArea.slice(0, cursorPosition) + tabValue
     + textFromTextArea.slice(cursorPosition);
 
@@ -214,18 +239,65 @@ function onKeyboardClickHandler(e) {
   addClassOfClickedItem(e);
   switchToUpperCase();
   switchTolowerCase();
-
-  if (queueFromClickedItems.length > 2) {
-    queueFromClickedItems.shift();
-  }
-  if (queueFromClickedItems[0] === 'Alt' && queueFromClickedItems[1] === 'Shift') {
-    changeLang();
-  }
 }
 
 keyboard.addEventListener('click', onKeyboardClickHandler);
 
-//  document.addEventListener('keydown', (e) => {
-//    console.log(e.repeat);
-//    console.log(e.code);
-// });
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  queueFromPressedKeys.push(e.code);
+  if (queueFromPressedKeys.length > 2) {
+    queueFromPressedKeys.shift();
+  }
+  if ((queueFromPressedKeys[0] === 'AltLeft' && queueFromPressedKeys[1] === 'ShiftLeft')
+  || (queueFromPressedKeys[1] === 'AltLeft' && queueFromPressedKeys[0] === 'ShiftLeft')) {
+    changeLang();
+    queueFromPressedKeys = [];
+  }
+
+  const arrayOfKeyBoardItems = document.querySelectorAll('.keyboard__item');
+  const k = keyCodes.indexOf(e.code);
+  arrayOfKeyBoardItems[k].classList.add('clicked');
+  setTimeout(delPreviousClickedItem, 250);
+
+  const cursorPosition = getCursor();
+  let textFromTextArea = textArea.value;
+  const pressedItem = arrayOfKeyBoardItems[k].querySelector('p').innerHTML;
+
+  if (!exceptionItems.includes(pressedItem)) {
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition) + pressedItem
+    + textFromTextArea.slice(cursorPosition - 1 + pressedItem.length);
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+  }
+  if (pressedItem === 'ENTER') {
+    const newLine = '\n';
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition) + newLine
+    + textFromTextArea.slice(cursorPosition - 1 + newLine.length);
+
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+  }
+  if (pressedItem === 'Backspace') {
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition - 1)
+    + textFromTextArea.slice(cursorPosition);
+
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition - 1, cursorPosition - 1);
+  }
+  if (pressedItem === 'DEL') {
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition)
+    + textFromTextArea.slice(cursorPosition + 1);
+
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition, cursorPosition);
+  }
+  if (pressedItem === 'Tab') {
+    const tabValue = '\t';
+    textFromTextArea = textFromTextArea.slice(0, cursorPosition) + tabValue
+    + textFromTextArea.slice(cursorPosition);
+
+    textArea.value = textFromTextArea;
+    textArea.setSelectionRange(cursorPosition + tabValue.length, cursorPosition + tabValue.length);
+  }
+});
